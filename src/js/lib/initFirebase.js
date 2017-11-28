2009f589f43febe675ebfdb74b17db02fc6857c0
nobii <fujisawa-shin@kayac.com>
2017-11-29 01:34:27 +0900

    firebaseのinitまで

---
+++ src/js/lib/initFirebase.js
@@ -0,0 +1,8 @@
+firebase.initializeApp({
+  apiKey: "AIzaSyBuVt6EMQG6hezPcQP40jtZliN2o5T5lEQ",
+  authDomain: "hand-bell.firebaseapp.com",
+  databaseURL: "https://hand-bell.firebaseio.com",
+  projectId: "hand-bell",
+  storageBucket: "",
+  messagingSenderId: "573098208437"
+});
