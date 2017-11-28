d62a0dea791ee36fbd040fa25fa67508c03e34cf
nobii <fujisawa-shin@kayac.com>
2017-11-29 01:48:15 +0900

    雑に認証

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,19 @@
 import initFirebase from './lib/initFirebase';
+
+let user;
+
+function init () {
+    firebase.auth().onAuthStateChanged((res) => {
+        user = res;
+        console.log('user', user);
+    });
+
+    firebase.auth().getRedirectResult();
+
+    document.querySelector('.js-button').addEventListener('click', () => {
+        const provider = new firebase.auth.GoogleAuthProvider();
+        firebase.auth().signInWithRedirect(provider);
+    });
+}
+
+init();
